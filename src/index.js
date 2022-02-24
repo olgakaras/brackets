8943e3fb76516377d76deb6e2f9471c1c8ecfa6d
olgakaras <olga.karas123@gmail.com>
2022-02-24 16:13:16 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+  let pairsArray = [];
+  let pairsCount = str.length / 2;
+  const escaping = ["[", "]", "^", "$", ".", "|", "?", "*", "+", "(", ")"];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let first = bracketsConfig[i][0];
+    if (escaping.includes(first)) first = "\\" + first;
+
+    let second = bracketsConfig[i][1];
+    if (escaping.includes(second)) second = "\\" + second;
+
+    pairsArray[i] = first + second;
+  }
+
+  let regExp = new RegExp(pairsArray.join("|"));
+
+  for (let i = 0; i < pairsCount; i++) {
+    let position = str.search(regExp);
+    if (position !== -1) {
+      str = str.slice(0, position) + str.slice(position + 2);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
